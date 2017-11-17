183dc20112fa9416a330b490afcf19d7f598475e
Schubert Rodriguez <scrodrig@thoughtworks.com>
2017-11-17 10:13:27 -0500

    SR [10] The root of All Evils

--- src/main/java/com/thoughtworks/tdd/example/money/Money.java
+++ src/main/java/com/thoughtworks/tdd/example/money/Money.java
@@ -1,6 +1,6 @@
 package com.thoughtworks.tdd.example.money;
 
-abstract class Money {
+ class Money {
 
     protected int amount;
     protected String currency;
@@ -11,19 +11,27 @@ abstract class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+     public Money times(int multiplier) {
+         return new Money(amount * multiplier, currency);
+     }
+
+    public String currency(){
+        return  currency;
     }
 
-    public abstract Money times(int multiplier);
+    public String toString() {
+        return amount + " " + currency;
+    }
 
-    public abstract String currency();
-}
+     public boolean equals(Object object) {
+         Money money = (Money) object;
+         return amount == money.amount && currency().equals(money.currency());
+     }
+ }
--- src/test/java/com/thoughtworks/tdd/example/money/MoneyTest.java
+++ src/test/java/com/thoughtworks/tdd/example/money/MoneyTest.java
@@ -1,7 +1,11 @@
 package com.thoughtworks.tdd.example.money;
 
+import junit.framework.Assert;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -24,4 +28,40 @@ public class MoneyTest {
     public void shouldBeEqualWhenCurrencyIsRequestedForFrancs(){
         assertThat(Money.franc(1).currency(), is("CHF"));
     }
+
+    @Test
+    public void shouldNotBeTrueWhenAmountAndCurrencyAreEqual() {
+        assertFalse(new Money(10, "CHF").equals(new Money(10, "USD")));
+    }
+
+    @Test
+    public void shouldReturnTheRightValueWhenYouMultiplyTheAmount() {
+        Money five = Money.dollar(5);
+        Money product = five.times(2);
+        MatcherAssert.assertThat(product, Matchers.is(Money.dollar(10)));
+    }
+
+    @Test
+    public void shouldReturnTheRightValueWhenMultiplyTheAmountAgain() {
+        Money five = Money.dollar(5);
+        Money ten = Money.dollar(10);
+        Money fifteen = Money.dollar(15);
+        MatcherAssert.assertThat(ten, Matchers.is(five.times(2)));
+        MatcherAssert.assertThat(fifteen, Matchers.is(five.times(3)));
+    }
+
+    @Test
+    public void shouldBeTrueWhenADollarEqualToAnother() {
+        Money five = Money.dollar(5);
+        Money anotherFive = Money.dollar(5);
+        assertTrue(five.equals(anotherFive));
+    }
+
+    @Test
+    public void shouldBeFalseWhenADollarIsNotEqualToAnother() {
+        Money five = Money.dollar(5);
+        Money six = Money.dollar(6);
+        Assert.assertFalse(five.equals(six));
+    }
+
 }
